53cc6e94df9da71519c503cf7da5f987107eaddb
mmr-p <morimoto3377@gmail.com>
2020-10-05 13:16:07 +0900

    Rubocopによるコード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name                  {'山田太郎'}
-    name_kana             {'ヤマダタロウ'}
-    employee_id           {'11111'}
-    email                 {Faker::Internet.free_email}
+    name                  { '山田太郎' }
+    name_kana             { 'ヤマダタロウ' }
+    employee_id           { '11111' }
+    email                 { Faker::Internet.free_email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
   end
