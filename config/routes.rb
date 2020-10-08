7829fce39a96d3edfb11ef2bc72b33f56cbc87ec
mmr-p <morimoto3377@gmail.com>
2020-10-08 13:30:58 +0900

    店舗情報編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'restaurants#top'
-  resources :restaurants, only: [:top, :index, :new, :create, :show]
+  resources :restaurants, only: [:top, :index, :new, :create, :show, :edit, :update]
 end
