9c917689a16ec7e203997855192ee9643ecdbda4
mmr-p <morimoto3377@gmail.com>
2020-10-15 17:32:37 +0900

    コード編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   ActionCable.server.config.disable_request_forgery_protection = true
-  config.action_cable.url = 'wss://13.230.255.170/cable'
+  config.action_cable.url = 'ws://13.230.255.170/cable'
   config.action_cable.allowed_request_origins = [ 'http://13.230.255.170', /http:\/\/13.230.255.170.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
