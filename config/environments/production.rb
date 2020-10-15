568183c5a886d7e5693a9623c766450f1c54acfd
mmr-p <morimoto3377@gmail.com>
2020-10-15 17:27:43 +0900

    ルーティングの追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://13.230.255.170', /http:\/\/13.230.255.170.*/ ]
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = 'wss://13.230.255.170/cable'
+  config.action_cable.allowed_request_origins = [ 'http://13.230.255.170', /http:\/\/13.230.255.170.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
