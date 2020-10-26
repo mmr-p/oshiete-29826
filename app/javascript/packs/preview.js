b19130077ed11afd5806b492de4508c32a561a94
mmr-p <morimoto3377@gmail.com>
2020-10-26 15:13:36 +0900

    プレビュー機能

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,43 @@
+if (document.URL.match( /users/ ) && document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('prof-image-list');
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('class', "edit-prof-img");
+      blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+    document.getElementById('user_image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
+  });
+} else if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('restaurant-image-list');
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('class', "restaurant-img");
+      blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+    document.getElementById('restaurant_image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
+  });
+}
