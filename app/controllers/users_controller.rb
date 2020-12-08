b04ecc6b7d0c7a976d4fff40b004004fd3e34d94
mmr-p <morimoto3377@gmail.com>
2020-12-09 02:13:13 +0900

    簡単ログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :test_user
   before_action :set_user
 
   def show
@@ -11,6 +11,18 @@ class UsersController < ApplicationController
 
   def update
     flash.now[:alert] = '更新に失敗しました' unless @user.update(user_params)
+  end
+
+  def test_user
+    user = User.find_or_create_by!(email: 'test@example.com') do |user|
+      user.name = "テストユーザー"
+      user.name_kana = "テストユーザー"
+      user.employee_id = "111"
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 
   private
