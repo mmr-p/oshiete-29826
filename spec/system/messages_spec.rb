5f6aff61ce18f8d987739e0b74babc9fea739224
mmr-p <morimoto3377@gmail.com>
2020-12-20 00:19:28 +0900

    メッセージの結合テスト

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -2,8 +2,36 @@ require 'rails_helper'
 
 RSpec.describe "Messages", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @message = FactoryBot.build(:message)
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'チャットを送信できる時' do
+    it 'ログインしたユーザーはチャットを送信できる' do
+      # ログイン
+      visit new_user_session_path
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq root_path
+      # チャットページへ遷移
+      visit messages_path
+      # チャットメッセージを送信する
+      fill_in 'message[content]', with: @message.content
+      find('#chat-send-btn').click
+      # リロードするとメッセージモデルが１上がる（非同期通信のためリロード要）
+      expect{
+        visit current_path
+      }.to change { Message.count }.by(1)
+      # ページにコメントした内容が存在することを確認
+      expect(page).to have_content(@message.content)
+    end
+  end
+  context 'チャットを送信できない時' do
+    it 'ログインしていないユーザーはチャットページに遷移しようとするとログインページへリダイレクトさせられる' do
+      # チャットページへ遷移
+      visit messages_path
+      # ログインページへリダイレクトされている
+      expect(current_path).to eq new_user_session_path
+    end
+  end
 end
